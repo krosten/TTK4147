98cea5b6e179c1d5ab852e84f8dac6ca36f4aeb3
Kristian Østensen <krisost@outlook.com>
2026-09-17 00:29:51 +0200

    Implementerte alle bortsettfra 1 av schedulerne, har ikke testet og sammenlignet med forventet virkemåte.

--- lab3/schedulers.c
+++ lab3/schedulers.c
@@ -74,20 +74,156 @@ void round_robin(struct Task **tasks, int taskCount, int timeout, int quantum)
 void first_come_first_served(struct Task **tasks, int taskCount, int timeout)
 {
     // Implement your solution here
+
+        // Her kommer første spor av kristian:
+    struct Task* taskToRun = tasks[0]; // Init task pointer
+
+    do{
+        // Find the earliest unfinished Task
+        for(int taskIndex = 1; taskIndex < taskCount; taskIndex++){
+            if(tasks[taskIndex]->state == finished || tasks[taskIndex]->arrivalTime > globalTime){
+                continue;
+            }
+
+            if(tasks[taskIndex]->arrivalTime < taskToRun->arrivalTime){
+                struct Task* taskToRun = tasks[taskIndex];
+            }
+        }
+
+        if(taskToRun == finished || taskToRun->arrivalTime > globalTime){ // New task was not available yet
+            continue;
+        }
+
+        if (taskToRun->startTime == -1)
+            taskToRun->startTime = globalTime; // Vet ikke hvorfor de gjør det i RR når det står at man ikke skal gjøre det i readme, men de gjør det i eksempelet så ¯\(ツ)/¯
+        set_task_state(taskToRun, running);
+
+        while(taskToRun->state != finished){} // Fant ingen form for signaling mekanisme uten quantum for at en task er ferdig, så her er en busy wait istedenfor, spør Joachim om noe finnes hvis det ikke funker
+
+    } while (globalTime < timeout);
 }
+
 void shortest_process_next(struct Task **tasks, int taskCount, int timeout)
 {
     // Implement your solution here
+    struct Task* taskToRun = tasks[0]; // Init task pointer
+
+    do{
+        // Find the shortest unfinished Task
+        for(int taskIndex = 1; taskIndex < taskCount; taskIndex++){
+            if(tasks[taskIndex]->state == finished || tasks[taskIndex]->arrivalTime > globalTime){
+                continue;
+            }
+
+            if(tasks[taskIndex]->totalRuntime < taskToRun->totalRuntime){
+                struct Task* taskToRun = tasks[taskIndex];
+            }
+        }
+
+        if(taskToRun == finished || taskToRun->arrivalTime > globalTime){ // New task was not available yet
+            continue;
+        }
+
+        if (taskToRun->startTime == -1)
+            taskToRun->startTime = globalTime;
+        set_task_state(taskToRun, running);
+
+        while(taskToRun->state != finished){} // Fant ingen form for signaling mekanisme uten quantum for at en task er ferdig, så her er en busy wait istedenfor, spør Joachim om noe finnes hvis det ikke funker
+
+    } while (globalTime < timeout);
 }
+
 void highest_response_ratio_next(struct Task **tasks, int taskCount, int timeout)
 {
     // Implement your solution here
+        // Highest response ratio = (W + S)/S
+        // W = hvor lenge en prosess har ventet
+        // S = Hvor lang tid det tar for prosessen å fullføre
+
+    struct Task* taskToRun = tasks[0]; // Init task pointer
+
+    do{
+        // Find the Task with highest ratio
+        for(int taskIndex = 1; taskIndex < taskCount; taskIndex++){
+            if(tasks[taskIndex]->state == finished || tasks[taskIndex]->arrivalTime > globalTime){
+                continue;
+            }
+
+            // Calculate ratios
+            int taskToRunW = globalTime - taskToRun->arrivalTime;
+            int taskToRunS = taskToRun->totalRuntime;
+            float taskToRunRatio = ((float)taskToRunW + (float)taskToRunS) / (float)taskToRunS;
+
+            int taskW = globalTime - tasks[taskIndex]->arrivalTime;
+            int taskS = tasks[taskIndex]->totalRuntime;
+            float taskRatio = ((float)taskW + (float)taskS) / (float)taskS;
+
+            if(taskToRunRatio < taskRatio){
+                struct Task* taskToRun = tasks[taskIndex];
+            }
+        }
+
+        if(taskToRun == finished || taskToRun->arrivalTime > globalTime){ // New task was not available yet
+            continue;
+        }
+
+        if (taskToRun->startTime == -1)
+            taskToRun->startTime = globalTime;
+        set_task_state(taskToRun, running);
+
+        while(taskToRun->state != finished){} // Fant ingen form for signaling mekanisme uten quantum for at en task er ferdig, så her er en busy wait istedenfor, spør Joachim om noe finnes hvis det ikke funker
+
+    } while (globalTime < timeout);
 }
 void shortest_remaining_time(struct Task **tasks, int taskCount, int timeout, int quantum)
 {
     // Implement your solution here
+
+    struct Task* taskToRun = tasks[0]; // Init task pointer
+
+    do{
+        // Find the Task with shortest time remaining
+        for(int taskIndex = 1; taskIndex < taskCount; taskIndex++){
+            if(tasks[taskIndex]->state == finished || tasks[taskIndex]->arrivalTime > globalTime){
+                continue;
+            }
+
+            // Calculate remaining time
+            int taskToRunRemaining = taskToRun->totalRuntime - taskToRun->currentRuntime;
+            int taskRemaining = tasks[taskIndex]->totalRuntime - tasks[taskIndex]->currentRuntime;
+
+            if(taskToRunRemaining > taskRemaining){
+                struct Task* taskToRun = tasks[taskIndex];
+            }
+        }
+
+        if(taskToRun == finished || taskToRun->arrivalTime > globalTime){ // New task was not available yet
+            continue;
+        }
+
+        if (taskToRun->startTime == -1)
+            taskToRun->startTime = globalTime;
+        set_task_state(taskToRun, running);
+
+        // Wait for the quantum interval
+        wait_for_rescheduling(quantum, taskToRun);
+
+        //  Check if the task is finished
+        if (taskToRun->state == finished)
+        {
+        }
+        else
+        {
+            set_task_state(taskToRun, preempted);
+        }
+
+    } while (globalTime < timeout);
 }
 void feedback(struct Task **tasks, int taskCount, int timeout, int quantum)
 {
     // Implement your solution here
+
+    // Har ikke laget forventninger av hvordan noen av schedulerene vil kjøres, så det må du gjøre for å sammenligne med output.
+    // Har ikke testet noe som helst btw, bare simulert det i hodet, så lykke til med debuggingen!
+    // Har også spart denne til deg, lykke til ;)
 }
